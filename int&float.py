cf867ca6abbc0149925d82b0ea12507f6be96579
nandinirajput <109655286+nandinirajput@users.noreply.github.com>
2022-07-21 20:35:13 +0530

    Add files via upload

---
+++ int&float.py
@@ -0,0 +1,33 @@
+ # a=123
+# print(type(a))
+# b=12.250
+# print(type(b))
+# c=1.025
+# print(type(c))
+# print(type(124520120))
+# a=5;b=4
+# print(a)
+# print('%d',%b)
+
+# e=4.23
+# print(e)
+# print('%f'%e)
+# L=1.225
+# print('%.1f'%L)
+# print('%.2f'%L)
+# print('%.3f'%L)
+# print('%.4f'%L)
+
+
+''' Type conversion & casting'''
+# m=5634
+# print(type(m))
+# print(float(m))
+# print('%d'%m)
+# print('%f'%m)
+# n=5.
+# print(type(n))
+# print(int(n))
+# print('%d'%n)
+# print('%f'%n)
+
